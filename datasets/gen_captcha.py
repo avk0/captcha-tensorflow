6e94360b60802391e38b5067851eba0c5bd5d84b
andrey <coval@texel.graphics>
2020-05-27 13:34:29 +0300

    gen_captcha.py add options: fonts, font sizes, img size, img format, max imgs num

--- datasets/gen_captcha.py
+++ datasets/gen_captcha.py
@@ -2,6 +2,7 @@
 import argparse
 import json
 import string
+from math import factorial
 import os
 import shutil
 import uuid
@@ -9,8 +10,14 @@ from captcha.image import ImageCaptcha
 
 import itertools
 
+
 FLAGS = None
 META_FILENAME = 'meta.json'
+FONTS = ['./fonts/arial.ttf', './fonts/smartie.ttf']  # if not set, default will be used
+FONT_SIZES = [62]  # if not set, default will be used
+WIDTH = 210
+HEIGHT = 60
+FORMAT = 'jpeg'
 
 
 def get_choices():
@@ -22,20 +29,39 @@ def get_choices():
     return tuple([i for is_selected, subset in choices for i in subset if is_selected])
 
 
-def _gen_captcha(img_dir, num_per_image, n, width, height, choices):
+def _gen_captcha(img_dir, num_per_image, n, e, fonts, font_sizes, width, height, choices):
+
+    def number_of_permutations(n, r):
+        return int(factorial(n) / factorial(n - r))
+
     if os.path.exists(img_dir):
         shutil.rmtree(img_dir)
     if not os.path.exists(img_dir):
         os.makedirs(img_dir)
 
-    image = ImageCaptcha(width=width, height=height)
+    image = ImageCaptcha(width=width, height=height, fonts=fonts, font_sizes=font_sizes)
 
-    print('generating %s epoches of captchas in %s' % (n, img_dir))
-    for _ in range(n):
+    # check if number of imgs is less than one epoch
+    permutations_n = number_of_permutations(len(choices), num_per_image)
+    print('Number of permutations %s, number needed %s' % (permutations_n, n))
+    if 0 < n < permutations_n:
+        e = 1
+        coeff = permutations_n // n
+    else:
+        coeff = 1
+
+    print('generating %s epoches of captchas in %s' % (e, img_dir))
+    print('images to be genetated:', e * permutations_n // coeff)
+    count = 0
+    for _ in range(e):
         for i in itertools.permutations(choices, num_per_image):
+            count += 1
+            if count % coeff != 0:
+                continue
             captcha = ''.join(i)
-            fn = os.path.join(img_dir, '%s_%s.png' % (captcha, uuid.uuid4()))
-            image.write(captcha, fn)
+            fn = os.path.join(img_dir, '%s_%s.%s' % (captcha, uuid.uuid4(), FORMAT))
+            print(fn)
+            image.write(captcha, fn, format=FORMAT)
 
 
 def build_file_path(x):
@@ -43,14 +69,12 @@ def build_file_path(x):
 
 
 def gen_dataset():
-    n_epoch = FLAGS.n
+    n_captchas = FLAGS.n
+    n_epoch = FLAGS.e
     num_per_image = FLAGS.npi
     test_ratio = FLAGS.t
 
     choices = get_choices()
-
-    width = 40 + 20 * num_per_image
-    height = 100
 
     # meta info
     meta = {
@@ -58,14 +82,18 @@ def gen_dataset():
         'label_size': len(choices),
         'label_choices': ''.join(choices),
         'n_epoch': n_epoch,
-        'width': width,
-        'height': height,
+        'width': WIDTH,
+        'height': HEIGHT,
+        'fonts': FONTS,
+        'font_sizes': FONT_SIZES
     }
 
     print('%s choices: %s' % (len(choices), ''.join(choices) or None))
 
-    _gen_captcha(build_file_path('train'), num_per_image, n_epoch, width, height, choices=choices)
-    _gen_captcha(build_file_path('test'), num_per_image, max(1, int(n_epoch * test_ratio)), width, height, choices=choices)
+    _gen_captcha(build_file_path('train'), num_per_image, n_captchas, n_epoch,
+                 FONTS, FONT_SIZES, WIDTH, HEIGHT, choices=choices)
+    _gen_captcha(build_file_path('test'), num_per_image, n_captchas, max(1, int(n_epoch * test_ratio)),
+                 FONTS, FONT_SIZES, WIDTH, HEIGHT, choices=choices)
 
     meta_filename = build_file_path(META_FILENAME)
     with open(meta_filename, 'w') as f:
@@ -78,6 +106,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '-n',
+        default=0,
+        type=int,
+        help='number of captchas to generate.')
+    parser.add_argument(
+        '-e',
         default=1,
         type=int,
         help='epoch number of character permutations.')
